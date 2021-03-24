8dacfea08f9274f8cb3a6b27847acf9bcc80df15
Sammmmmuel <69543188+Sammmmmuel@users.noreply.github.com>
2021-03-23 22:36:30 -0700

    added lyrics

--- script.js
+++ script.js
@@ -4,11 +4,34 @@ const originText = document.querySelector('.text').innerHTML
 const resetButton = document.querySelector('#reset')
 const theTimer = document.querySelector('.timer')
 
+const lyrics = [
+    `Somebody once told me the world is gonna roll me I ain't the sharpest tool in the shed`,
+    `My milkshake brings all the boys to the yard And they're like, it's better than yours Damn right it's better than yours`,
+    `I walk a lonely road The only one that I have ever known Don't know where it goes But it's home to me, and I walk alone`,
+    `No, I don't want no scrub A scrub is a guy that can't get no love from me Hangin' out the passenger side of his best friend's ride Tryin' to holla at me`,
+    `I been tryin' to do it right I been livin' a lonely life I been sleepin' here instead I been sleepin' in my bed I been sleepin' in my bed`,
+    `Stacy's mom has got it goin' on She's all I want And I've waited for so long Stacy, can't you see? You're just not the girl for me`,
+    `Woah, we're half way there Woah, livin' on a prayer Take my hand, we'll make it I swear Woah, livin' on a prayer`,
+    `You would not believe your eyes If ten million fireflies Lit up the world as I fell asleep`,
+    `I'm hooked on a feeling I'm high on believing That you're in love with me`,
+    `If you like piña coladas And gettin' caught in the rain If you're not into yoga If you have half a brain If you like makin' love at midnight`,
+    `Lean on me When you're not strong And I'll be your friend I'll help you carry on... For it won't be long Till I'm gonna need somebody to lean on`,
+    `Take me to your best friend's house Roll around this roundabout Oh yeah Take me to your best friend's house`,
+    `Someone told me long ago There's a calm before the storm I know, it's been comin' for some time When it's over, so they say It'll rain a sunny day`,
+    `Hey baby, won't you look my way? I can be your new addiction Hey baby, what you gotta say? All you're giving me is fiction`,
+    `Hey, soul sister Ain't that Mr. Mister on the radio, stereo The way you move ain't fair, you know`,
+    `Hey there, Delilah What's it like in New York city? I'm a thousand miles away But, girl, tonight you look so pretty Yes, you do`,
+    `I was scared of dentists and the dark I was scared of pretty girls and starting conversations Oh, all my friends are turning green`,
+    `Well, you done done me and you bet I felt it I tried to be chill, but you're so hot that I melted I fell right through the cracks Now I'm trying to get back`,
+    `Big wheels keep on turnin' Carry me home to see my kin Singin' songs about the south-land I miss Alabamy once again and I think it's a sin, yes`,
+    `Sweet Caroline Good times never seemed so good I've been inclined To believe they never would But now I`,
+    `Uh, summa-lumma, dooma-lumma, you assumin' I'm a human What I gotta do to get it through to you I'm superhuman?`,
 
-// new code 
+]
+
 
 const typingDiv = document.getElementById('typing')
-const text = originText
+const text = lyrics[parseInt(Math.random() * lyrics.length)]
     // typingDiv.innerText = text
 let cursorIndex = 0
 const characters = text.split('').map((char) => {
@@ -37,7 +60,6 @@ const keyListener = document.addEventListener('keydown', ({ key }) => {
         endTime = new Date()
         const delta = endTime - startTime
         const seconds = delta / 1000
-
         const numberOfWords = text.split(" ").length
         const wps = numberOfWords / seconds
         const wpm = Math.round(wps * 60.0)
@@ -46,12 +68,7 @@ const keyListener = document.addEventListener('keydown', ({ key }) => {
         return
     }
     cursorCharacter.classList.add('cursor')
-
 })
-
-// new code 
-
-
 
 let timer = [0, 0, 0, 0]
 let interval
@@ -64,7 +81,6 @@ function leadingZero(time) {
     return time
 }
 
-
 function runTimer() {
     let currentTime = leadingZero(timer[0]) + ":" + leadingZero(timer[1]) + ":" + leadingZero(timer[2])
     theTimer.innerHTML = currentTime
@@ -74,7 +90,6 @@ function runTimer() {
     timer[1] = Math.floor((timer[3] / 100) - (timer[0] * 60))
     timer[2] = Math.floor(timer[3] - (timer[1] * 100) - (timer[0] * 6000))
 }
-
 
 function reset() {
     clearInterval(interval)
@@ -89,9 +104,9 @@ function reset() {
 
 function spellCheck() {
     let textEntered = testArea.value
-    let originTextMatch = originText.substring(0, textEntered.length)
+    let originTextMatch = text.substring(0, textEntered.length)
 
-    if (textEntered == originText) {
+    if (textEntered == text) {
         testWrapper.style.borderColor = 'orange';
         clearInterval(interval)
     } else {
@@ -109,11 +124,8 @@ function start() {
         timerRunning = true
         interval = setInterval(runTimer, 10)
     }
-
 }
-
-
 
 testArea.addEventListener('keypress', start, false)
 testArea.addEventListener('keyup', spellCheck, false)
-resetButton.addEventListener('click', reset, false)
+    // resetButton.addEventListener('click', reset, false)
